4d00e66b0d3212f76de0fa702fb00a7326fec45a
Asato TAKAHASHI <asato730@gmail.com>
2021-04-14 21:21:38 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Team Timer</h1>
+    <button @click="createRoom">ルームをつくる</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    createRoom() {
+      console.log("hello")
+    }
+  }
+}
+</script>
+
+<style>
+</style>
